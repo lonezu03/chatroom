d74bd0da4401777eb3c396029984f0c4bd1ee651
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2023-12-14 23:39:10 +0700

    14/12-11:39

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express =require('express')
 const app=express()
 
+const fs = require('fs');
+const path = require('path');
+
 const http=require('http')
 const server =http.createServer(app)
 const {
@@ -13,11 +16,30 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
     //res.sendFile(__dirname+'/style.css');
 })
+function saveFile(fileData) {
+    const fileBuffer = Buffer.from(fileData.data, 'base64');
+    const filePath = path.join(__dirname, 'uploads', fileData.name);
+  
+    fs.writeFile(filePath, fileBuffer, (err) => {
+      if (err) {
+        console.error('Error saving file:', err);
+      } else {
+        console.log('File saved:', fileData.name);
+      }
+    });
+  }
 io.on('connection',(socket)=>{
     console.log('user connected' )
     socket.on('on-chat',data=>{
     io.emit('user-chat',data)
     })
+ 
+      socket.on('file-upload', (fileData) => {
+        saveFile(fileData);
+    
+        // Gửi lại file cho tất cả các client khác
+        socket.broadcast.emit('file-available', fileData.name);
+      });
 })
 
 server.listen(3000,()=>{
